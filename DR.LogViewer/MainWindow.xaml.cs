8b162408a11c8008272d292b7f7c563948aa3118
Jongho Yang <jongho.yang@gmail.com>
2020-12-27 14:12:28 +0900

    fefe

--- DR.LogViewer/MainWindow.xaml.cs
+++ DR.LogViewer/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,25 +14,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace DR.LogViewer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        public CultureInfo DateTimeCulture
+        {
+            get
+            {
+                var culture = new CultureInfo("en-IN");
+                culture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+                culture.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
+                return culture;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            //Thread.CurrentThread.CurrentCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            //Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            //Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            pathText.Text = folder;
-            System.IO.File.WriteAllText($@"{folder}\WriteText.txt", DateTime.Now.ToLongTimeString());
-
+ 
         }
     }
 }
